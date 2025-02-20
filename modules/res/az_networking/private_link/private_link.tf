5fd770da2df14f53298bc68390e3fcd319bc0142
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-20 12:47:55 +0100

    private dns

--- modules/res/az_networking/private_link/private_link.tf
+++ modules/res/az_networking/private_link/private_link.tf
@@ -1,7 +1,9 @@
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
-  name                  = "dns-link"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.private_dns.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-  registration_enabled = true
-}
+# resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+#   for_each = 
+  
+#   name                  = "dns-link"
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   private_dns_zone_name = azurerm_private_dns_zone.private_dns.name
+#   virtual_network_id    = azurerm_virtual_network.vnet.id
+#   registration_enabled = true
+# }
