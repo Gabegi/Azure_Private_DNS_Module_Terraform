9cb04a9be12df3bf09495b92a588c189958f369f
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-21 12:16:37 +0100

    pattern

--- modules/ptn/dns/main.tf
+++ modules/ptn/dns/main.tf
@@ -1,10 +1,10 @@
 module "private_dns" {
-  source = "./res/az_networking/private_dns_zone"
+  source = "../../res/az_networking/private_dns_zone"
   rg_name = module.general.rg_name
   private_dns = var.private_dns
 }
 
 module "private_link" {
-  source = "./res/az_networking/private_link"
+  source = "../../res/az_networking/private_link"
   rg_name = module.general.rg_name
 }
